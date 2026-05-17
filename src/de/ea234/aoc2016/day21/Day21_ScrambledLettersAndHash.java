c19d118f1acef4c93be0414c6607d0068b0207b8
ea234 <anyang@gmx.de>
2026-05-17 14:16:30 +0200

    Day 21 - Part 1

--- src/de/ea234/aoc2016/day21/Day21_ScrambledLettersAndHash.java
+++ src/de/ea234/aoc2016/day21/Day21_ScrambledLettersAndHash.java
@@ -15,12 +15,53 @@ import java.util.stream.Collectors;
  * 
  * https://www.reddit.com/r/adventofcode/comments/5ji29h/2016_day_21_solutions/
  * 
+ * ----------------------------------------------------------------------
+ * 0123456789 0823456719 CMD swap position 1 with position 8
+ * 0123456789 9123456780 CMD swap position 0 with position 9
+ * 
+ * AAACCCBBBB BBBCCCAAAA CMD swap letter A with letter B
+ * 
+ * 0123456789 9012345678 CMD rotate right 1 step
+ * 0123456789 8901234567 CMD rotate right 2 step
+ * 
+ * 0123456789 1234567890 CMD rotate left 1 step
+ * 0123456789 2345678901 CMD rotate left 2 step
+ * 
+ * abdec ecabd CMD rotate based on position of letter b
+ * ecabd decab CMD rotate based on position of letter d
+ * 
+ * bcdea bdeac CMD move position 1 to position 4
+ * bdeac abdec CMD move position 3 to position 0
+ * 
+ * 0123456789 4321056789 CMD reverse positions 0 through 4
+ * 
+ * ----------------------------------------------------------------------
+ *  abcde  |  
+ *  ebcda  |  swap position 4 with position 0
+ *  edcba  |  swap letter d with letter b
+ *  abcde  |  reverse positions 0 through 4
+ *  bcdea  |  rotate left 1 step
+ *  bdeac  |  move position 1 to position 4
+ *  abdec  |  move position 3 to position 0
+ *  ecabd  |  rotate based on position of letter b
+ *  decab  |  rotate based on position of letter d
+ * 
+ * Result Part 1 decab
+ * Result Part 2 0
+ * 
+ * 
+ * ------------------------------------------------------------------------------------------
+ * Result Part 1 aefgbcdh
+ * Result Part 2 0
+ * 
  * </pre> 
  */
 public class Day21_ScrambledLettersAndHash
 {
   public static void main( String[] args )
   {
+    test();
+
     String test_input = "";
 
     test_input += ",swap position 4 with position 0";
@@ -34,7 +75,7 @@ public class Day21_ScrambledLettersAndHash
 
     calculatePart01( test_input, "abcde", true );
 
-    //calculate01( getListProd(), "abcdefgh" false );
+    calculate01( getListProd(), "abcdefgh", false );
 
     System.exit( 0 );
   }
@@ -48,104 +89,232 @@ public class Day21_ScrambledLettersAndHash
 
   private static void calculate01( List< String > pListInput, String pStart, boolean pKnzDebug )
   {
+    wl( "" );
+    wl( "----------------------------------------------------------------------" );
+
     byte[] password = pStart.getBytes();
 
-    int result_part_01 = 0;
     int result_part_02 = 0;
 
     for ( String input_str : pListInput )
     {
-      if ( input_str.isBlank() )
-      {
-      }
-      else if ( input_str.startsWith( "swap position " ) )
-      {
-        int position_x = ( (int) input_str.charAt( 14 ) ) - 48;
-        int position_y = ( (int) input_str.charAt( 30 ) ) - 48;
+      doCmd( password, input_str );
 
-        byte temp = password[ position_x ];
-
-        password[ position_x ] = password[ position_y ];
-
-        password[ position_y ] = temp;
-
-        wl( "swap position X with position Y  -  " + position_x + " " + position_y );
-      }
-      else if ( input_str.startsWith( "swap letter " ) )
-      {
-        byte ascii_x = (byte) ( (int) input_str.charAt( 12 ) );
-        byte ascii_y = (byte) ( (int) input_str.charAt( 26 ) );
-
-        for ( int idx = 0; idx < password.length; idx++ )
-        {
-          if ( password[ idx ] == ascii_x )
-          {
-            password[ idx ] = ascii_y;
-          }
-          else if ( password[ idx ] == ascii_y )
-          {
-            password[ idx ] = ascii_x;
-          }
-        }
-
-        wl( "swap letter X with letter Y" );
-      }
-      else if ( input_str.startsWith( "rotate left " ) )
-      {
-        int x_steps = ( (int) input_str.charAt( 12 ) ) - 48;
-
-        for ( int step_counter = 0; step_counter < x_steps; step_counter++ )
-        {
-          byte save_first_byte = password[ 0 ];
-
-          for ( int idx = 0; idx < ( password.length - 1 ); idx++ )
-          {
-            password[ idx ] = password[ idx + 1 ];
-          }
-
-          password[ password.length - 1 ] = save_first_byte;
-        }
-
-        wl( "rotate left X steps" );
-      }
-      else if ( input_str.startsWith( "rotate right " ) )
-      {
-        int x_steps = ( (int) input_str.charAt( 13 ) ) - 48;
-
-        for ( int step_counter = 0; step_counter < x_steps; step_counter++ )
-        {
-          byte save_first_byte = password[ password.length - 1 ];
-
-          for ( int idx = password.length - 1; idx > 0; idx-- )
-          {
-            password[ idx ] = password[ idx - 1 ];
-          }
-
-          password[ 0 ] = save_first_byte;
-        }
-
-        wl( "rotate right X steps" );
-      }
-      else if ( input_str.startsWith( "move position " ) )
-      {
-        wl( "move position X to position Y" );
-      }
-      else if ( input_str.startsWith( "rotate based on position of letter" ) )
-      {
-        wl( "rotate based on position of letter X" );
-      }
-      else if ( input_str.startsWith( "reverse positions " ) )
-      {
-        wl( "reverse positions X through Y" );
-      }
+      wl( " " + ( new String( password ) ) + "  |  " + input_str );
     }
 
-    wl( new String( password ) );
     wl( "" );
-    wl( "------------------------------------------------------------------------------------------" );
-    wl( "Result Part 1 " + result_part_01 );
+    wl( "Result Part 1 " + new String( password ) );
     wl( "Result Part 2 " + result_part_02 );
     wl( "" );
+  }
+
+  private static void test()
+  {
+    String test_input = "0123456789";
+
+    wl( "" );
+    wl( "----------------------------------------------------------------------" );
+
+    doTestCmd( test_input, "swap position 1 with position 8" );
+    doTestCmd( test_input, "swap position 0 with position 9" );
+
+    wl( "" );
+    doTestCmd( "AAACCCBBBB", "swap letter A with letter B" );
+
+    wl( "" );
+    doTestCmd( test_input, "rotate right 1 step" );
+    doTestCmd( test_input, "rotate right 2 step" );
+
+    wl( "" );
+    doTestCmd( test_input, "rotate left 1 step" );
+    doTestCmd( test_input, "rotate left 2 step" );
+
+    wl( "" );
+    doTestCmd( "abdec", "rotate based on position of letter b" );
+    doTestCmd( "ecabd", "rotate based on position of letter d" );
+
+    wl( "" );
+    doTestCmd( "bcdea", "move position 1 to position 4" );
+    doTestCmd( "bdeac", "move position 3 to position 0" ); // abdec
+
+    wl( "" );
+    doTestCmd( "0123456789", "reverse positions 0 through 4" );
+  }
+
+  private static void doTestCmd( String pString, String pCmd )
+  {
+    byte[] byte_array = pString.getBytes();
+
+    doCmd( byte_array, pCmd );
+
+    wl( pString + " " + ( new String( byte_array ) ) + " CMD " + pCmd );
+  }
+
+  private static void doCmd( byte[] pByteArray, String pCmd )
+  {
+    if ( pCmd.isBlank() )
+    {
+    }
+    else if ( pCmd.startsWith( "swap position " ) )
+    {
+      int position_x = ( (int) pCmd.charAt( 14 ) ) - 48;
+      int position_y = ( (int) pCmd.charAt( 30 ) ) - 48;
+
+      byte temp = pByteArray[ position_x ];
+
+      pByteArray[ position_x ] = pByteArray[ position_y ];
+
+      pByteArray[ position_y ] = temp;
+    }
+    else if ( pCmd.startsWith( "swap letter " ) )
+    {
+      byte ascii_x = (byte) ( (int) pCmd.charAt( 12 ) );
+      byte ascii_y = (byte) ( (int) pCmd.charAt( 26 ) );
+
+      for ( int idx = 0; idx < pByteArray.length; idx++ )
+      {
+        if ( pByteArray[ idx ] == ascii_x )
+        {
+          pByteArray[ idx ] = ascii_y;
+        }
+        else if ( pByteArray[ idx ] == ascii_y )
+        {
+          pByteArray[ idx ] = ascii_x;
+        }
+      }
+    }
+    else if ( pCmd.startsWith( "rotate left " ) )
+    {
+      int x_steps = ( (int) pCmd.charAt( 12 ) ) - 48;
+
+      for ( int step_counter = 0; step_counter < x_steps; step_counter++ )
+      {
+        byte save_first_byte = pByteArray[ 0 ];
+
+        for ( int idx = 0; idx < ( pByteArray.length - 1 ); idx++ )
+        {
+          pByteArray[ idx ] = pByteArray[ idx + 1 ];
+        }
+
+        pByteArray[ pByteArray.length - 1 ] = save_first_byte;
+      }
+    }
+    else if ( pCmd.startsWith( "rotate right " ) )
+    {
+      int x_steps = ( (int) pCmd.charAt( 13 ) ) - 48;
+
+      for ( int step_counter = 0; step_counter < x_steps; step_counter++ )
+      {
+        byte save_first_byte = pByteArray[ pByteArray.length - 1 ];
+
+        for ( int idx = pByteArray.length - 1; idx > 0; idx-- )
+        {
+          pByteArray[ idx ] = pByteArray[ idx - 1 ];
+        }
+
+        pByteArray[ 0 ] = save_first_byte;
+      }
+    }
+    else if ( pCmd.startsWith( "move position " ) )
+    {
+      int index_start = ( (int) pCmd.charAt( 14 ) ) - 48;
+      int index_end   = ( (int) pCmd.charAt( 28 ) ) - 48;
+
+      byte save_first_byte = pByteArray[ index_start ];
+
+      int idx = index_start;
+
+      if ( index_start < index_end )
+      {
+        while ( idx != index_end )
+        {
+          pByteArray[ idx ] = pByteArray[ incIndex( idx, pByteArray.length ) ];
+
+          idx = incIndex( idx, pByteArray.length );
+        }
+      }
+      else
+      {
+        while ( idx != index_end )
+        {
+          pByteArray[ idx ] = pByteArray[ decIndex( idx, pByteArray.length ) ];
+
+          idx = decIndex( idx, pByteArray.length );
+        }
+      }
+
+      pByteArray[ index_end ] = save_first_byte;
+    }
+    else if ( pCmd.startsWith( "rotate based on position of letter" ) )
+    {
+      byte ascii_letter = (byte) ( (int) pCmd.charAt( 35 ) );
+
+      int letter_index = 0;
+
+      while ( ( letter_index < pByteArray.length ) && ( pByteArray[ letter_index ] != ascii_letter ) )
+      {
+        letter_index++;
+      }
+
+      int rotate_one_time = 1;
+
+      int rotate_additional_one_time = ( letter_index >= 4 ? 1 : 0 );
+
+      int x_steps = rotate_one_time + letter_index + rotate_additional_one_time;
+
+      for ( int step_counter = 0; step_counter < x_steps; step_counter++ )
+      {
+        byte save_first_byte = pByteArray[ pByteArray.length - 1 ];
+
+        for ( int idx = pByteArray.length - 1; idx > 0; idx-- )
+        {
+          pByteArray[ idx ] = pByteArray[ idx - 1 ];
+        }
+
+        pByteArray[ 0 ] = save_first_byte;
+      }
+    }
+    else if ( pCmd.startsWith( "reverse positions " ) )
+    {
+      int index_start = ( (int) pCmd.charAt( 18 ) ) - 48;
+      int index_end   = ( (int) pCmd.charAt( 28 ) ) - 48;
+
+      if ( index_end < index_start )
+      {
+        int index_save = index_end;
+
+        index_end = index_start;
+
+        index_start = index_save;
+      }
+
+      while ( ( index_start < index_end ) && ( index_start != index_end ) )
+      {
+        byte temp = pByteArray[ index_end ];
+
+        pByteArray[ index_end ] = pByteArray[ index_start ];
+
+        pByteArray[ index_start ] = temp;
+
+        index_end--;
+        index_start++;
+      }
+    }
+  }
+
+  private static int incIndex( int pIndex, int pMaxIndex )
+  {
+    if ( ( pIndex + 1 ) >= pMaxIndex ) return 0;
+
+    return pIndex + 1;
+  }
+
+  private static int decIndex( int pIndex, int pMaxIndex )
+  {
+    if ( ( pIndex - 1 ) < 0 ) return pMaxIndex - 1;
+
+    return pIndex - 1;
   }
 
   private static List< String > getListProd()
