0177a0dadb7bda9e8df13cc831dcdebab8d1ad75
ea234 <anyang@gmx.de>
2026-07-16 10:51:08 +0200

    Day 03 - Part 2 - 1

--- src/de/ea234/aoc2016/day03/Day03_SquaresWithThreeSides.java
+++ src/de/ea234/aoc2016/day03/Day03_SquaresWithThreeSides.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
  * 
  * https://www.reddit.com/r/adventofcode/comments/5g80ck/2016_day_3_solutions/
  * 
- * 
+ * https://github.com/ea234/Advent_of_Code_2016/blob/main/src/de/ea234/aoc2016/day03/Day03_SquaresWithThreeSides.java
  * </pre>
  */
 public class Day03_SquaresWithThreeSides
@@ -41,18 +41,71 @@ public class Day03_SquaresWithThreeSides
   private static void calculate01( List< String > pListInput, boolean pKnzDebug )
   {
     int result_part_01 = 0;
-    
-    String result_part_02 = "";
-    
+
+    int result_part_02 = 0;
+
+    List< Integer > x_col_a = new ArrayList< Integer >();
+    List< Integer > x_col_b = new ArrayList< Integer >();
+    List< Integer > x_col_c = new ArrayList< Integer >();
+
     for ( String input_str : pListInput )
     {
-      int triangle_ok = checkTriangle( input_str );
-      
+      int triangle_ok = 1;
+
+      if ( !input_str.isBlank() )
+      {
+        List< String > triangle = Arrays.stream( input_str.trim().replaceAll( " {2,}", " " ).split( " " ) ).map( String::trim ).collect( Collectors.toList() );
+
+        int side_a = Integer.parseInt( triangle.get( 0 ) );
+        int side_b = Integer.parseInt( triangle.get( 1 ) );
+        int side_c = Integer.parseInt( triangle.get( 2 ) );
+
+        if ( ( side_a + side_b ) <= side_c )
+        {
+          triangle_ok = 0;
+        }
+        else if ( ( side_a + side_c ) <= side_b )
+        {
+          triangle_ok = 0;
+        }
+        else if ( ( side_c + side_b ) <= side_a )
+        {
+          triangle_ok = 0;
+        }
+
+        x_col_a.add( Integer.valueOf( side_a ) );
+        x_col_b.add( Integer.valueOf( side_b ) );
+        x_col_c.add( Integer.valueOf( side_c ) );
+      }
+
       wl( input_str + "    " + ( triangle_ok == 1 ? " OK " : "" ) );
-      
-      result_part_01 += triangle_ok; 
+
+      result_part_01 += triangle_ok;
     }
 
+    int count_a = countX( x_col_a );
+    int count_b = countX( x_col_b );
+    int count_c = countX( x_col_c );
+
+    wl( "" );
+    wl( "count_a " + count_a );
+    wl( "count_b " + count_b );
+    wl( "count_c " + count_c );
+
+    x_col_a.sort( null );
+    x_col_b.sort( null );
+    x_col_c.sort( null );
+
+    count_a = countX( x_col_a );
+    count_b = countX( x_col_b );
+    count_c = countX( x_col_c );
+
+    result_part_02 = count_a + count_b + count_c;
+
+    wl( "" );
+    wl( "count_a " + count_a );
+    wl( "count_b " + count_b );
+    wl( "count_c " + count_c );
     wl( "" );
     wl( "Result Part 1 " + result_part_01 );
     wl( "Result Part 2 " + result_part_02 );
@@ -60,23 +113,59 @@ public class Day03_SquaresWithThreeSides
     wl( "" );
   }
 
-  public static int checkTriangle( String pInput )
+  private static int countX1( List< Integer > pVector )
   {
-    if ( pInput.isBlank() ) return 0;
-    
-    List< String > triangle = Arrays.stream( pInput.trim().replaceAll(" {2,}", " ").split( " " ) ).map( String::trim ).collect( Collectors.toList() );
+    int count_nr = 0;
 
-    int side_a = Integer.parseInt( triangle.get( 0 ) );
-    int side_b = Integer.parseInt( triangle.get( 1 ) );
-    int side_c = Integer.parseInt( triangle.get( 2 ) );
+    int idx = 0;
 
-    if ( ( side_a + side_b ) <= side_c ) { return 0; }
-    if ( ( side_a + side_c ) <= side_b ) { return 0; }
-    if ( ( side_c + side_b ) <= side_a ) { return 0; }
-    
-    return 1;    
+    while ( idx < pVector.size() - 2 )
+    {
+      int mod_100_a = ( pVector.get( idx ).intValue() / 100 ) % 100;
+      int mod_100_b = ( pVector.get( idx + 1 ).intValue() / 100 ) % 100;
+      int mod_100_c = ( pVector.get( idx + 2 ).intValue() / 100 ) % 100;
+
+      if ( ( mod_100_a == mod_100_b ) && ( mod_100_a == mod_100_c ) && ( mod_100_b == mod_100_c ) )
+      {
+        count_nr++;
+
+        idx += 3;
+      }
+      else
+      {
+        idx++;
+      }
+    }
+
+    return count_nr;
   }
 
+  private static int countX( List< Integer > pVector )
+  {
+    int count_nr = 0;
+
+    int idx = 0;
+
+    while ( idx < pVector.size() - 2 )
+    {
+      int mod_100_a = ( pVector.get( idx ).intValue() / 100 ) % 100;
+      int mod_100_b = ( pVector.get( idx + 1 ).intValue() / 100 ) % 100;
+      int mod_100_c = ( pVector.get( idx + 2 ).intValue() / 100 ) % 100;
+
+      if ( ( mod_100_a == mod_100_b ) && ( mod_100_a == mod_100_c ) && ( mod_100_b == mod_100_c ) )
+      {
+        count_nr++;
+
+        idx += 3;
+      }
+      else
+      {
+        idx += 3;
+      }
+    }
+
+    return count_nr;
+  }
 
   private static List< String > getListProd()
   {
